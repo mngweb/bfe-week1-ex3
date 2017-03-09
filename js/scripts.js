6cfc65bb5daa71c0a4dcb704b14e92dff7d95719
mngweb <mangoweb@wp.pl>
2017-03-09 13:50:15 +0100

    Zadanie 1.3

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+(function($) {
+
+    $(document).ready(function() {
+
+        var nav = $(".nav-main");
+
+
+        $(".toggle-menu").on("click", function() {
+
+            var isVisible = nav.is(":visible"),
+                navWidth = nav.outerWidth(); 
+
+            $(this).toggleClass('click', !isVisible);
+
+
+            nav.stop().slideToggle(500).transit({
+                //opacity: "toggle",       //nie działa?
+                opacity: !isVisible ? 1 : 0,
+                left: !isVisible ? 0 : -navWidth
+                }, {
+                duration: 500,
+                //easing: "easeOutBounce",      //nie działa?
+                queue: false  
+            }); 
+
+
+        });
+
+    });
+
+})(jQuery);
+
+
